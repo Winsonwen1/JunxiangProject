2a0ddd81082aebef40db331ba612e073c0280760
winsonwen <54719970+winsonwen@users.noreply.github.com>
2019-11-03 23:51:24 -0500

    add all test file
    test file

--- Project3/testCharSeek.c
+++ Project3/testCharSeek.c
@@ -8,52 +8,60 @@
 #include<stdio.h>
 #include<string.h>          //Func :memset
 
-#define BUFFER_LENGTH 1024	// max of string length
-#define MAX_CONN_LIMIT 512     //MAX connection limit
+#define MAX_BUFFER 1024	// max of string length
+
 
 
 int main() {
-	//initial 
-	char  data_recv[BUFFER_LENGTH] = "test" ;
+	//initial
+	char inputD[MAX_BUFFER]= {};
 	FILE *fp;
-	char inputD[BUFFER_LENGTH]= {};
+	char dataSend[MAX_BUFFER]= {};
 	fp = fopen("words.txt", "r");
 	char c;
 
 	int k=0 ;
 
 	while(1) {
+		printf("Type a words\n");
+		fgets(dataSend, sizeof(char)*MAX_BUFFER, stdin);
 
-		c = fgetc(fp);
-		//	printf("%c\n",c);
-		if( c==10 || c==EOF) {
-			if(c==EOF) {
+		for(int x=0; dataSend[x]!='\0'; x++) {
+			if(dataSend[x]==10)
+				dataSend[x]='\0';
+		}
+		fseek(fp,0,SEEK_SET);
+		while(1) {
+			c = fgetc(fp);
+			//	printf("%c\n",c);
+			if( c==10 || c==EOF) {
+				if(c==EOF) {
+					inputD[k] ='\0';
+					k=0;
+					if(strcmp(inputD,dataSend)==0) {
+						printf("%s  have char  %s\n", inputD, dataSend);
+						break;
+					}
+//					fclose(fp);
+					printf("No char\n");
+					break;
+				}
 				inputD[k] ='\0';
 				k=0;
-				if(strcmp(inputD,data_recv)==0) {
-				printf("have char\n");
-					return 1;
+				if(strcmp(inputD,dataSend)==0) {
+					printf("%s  have char  %s\n", inputD, dataSend);
+//					fclose(fp);
+					break;
 				}
-				fclose(fp);
-				printf("No char\n");
-				return 0;
+				continue;
+			} else {
+				inputD[k] =c;
+				k++;
+				continue;
 			}
-			inputD[k] ='\0';
-			k=0;
-			if(strcmp(inputD,data_recv)==0) {
-				printf("%s  have char  %s\n", inputD, data_recv);
-				fclose(fp);
-				return 1;
-			}
-			continue;
-		} else {
-			inputD[k] =c;
-			k++;
-			continue;
 		}
+
 	}
-
-
 
 	printf("error1\n");
 	fclose(fp);
